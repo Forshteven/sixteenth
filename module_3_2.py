e15177477f87eb2e80d450e5f7e9f0302a188c62
Kosikhin Nikolay <geocinds@mail.ru>
2025-03-07 17:31:47 +0900

    более красивый код

--- module_3_2.py
+++ module_3_2.py
@@ -1,23 +1,16 @@
-def send_email(message, recipient, sender = "university.help@gmail.com"):
-    a = "@"
-    b = ".com"
-    c = ".ru"
-    d = ".net"
-    e = sender
-    f = recipient
-    if e.find(a) == -1 or f.find(a) == -1:
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    elif e.find(b) == -1 and e.find(c) == -1 and e.find(d) == -1:
-            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    elif f.find(b) == -1 and f.find(c) == -1 and f.find(d) == -1:
-            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    elif sender == recipient:
-        print(f"Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+def send_email(message, recipient='university@mail.ru', sender='university.help@gmail.com'):
+    if sender == recipient:
+        print(f'Нельзя отправить письмо самому себе!')
+    elif '@' not in recipient and sender:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender.endswith('.com' or '.ru' or '.net') == -1:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif recipient.endswith('.com' or '.ru' or '.net') == -1:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
     else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
 
 send_email("Это сообщение для проверки связи", "vasyok1337@gmail.com")
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
